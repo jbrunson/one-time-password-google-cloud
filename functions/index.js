081da0657b4b3ea0a811e46988190b940a91b0c7
Jeff Brunson <jeff.a.brunson@gmail.com>
2017-04-15 11:12:49 -0600

    initial setup

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const createUser = require('./create_user');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-a53fd.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
+
